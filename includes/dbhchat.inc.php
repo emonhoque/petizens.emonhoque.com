6a7d7a96713cf6568b49e5b58d931d9b6fbc92d3
emonhoque <56671915+emonhoque@users.noreply.github.com>
2023-11-18 12:29:19 -0500

    Update dbhchat.inc.php

--- includes/dbhchat.inc.php
+++ includes/dbhchat.inc.php
@@ -2,6 +2,6 @@
 
 //database_connection_chat
 
-$conn = new PDO("mysql:host=localhost;dbname=petizens;charset=utf8mb4", "root", "4Eo60fsximxIA7Dy");
+$conn = new PDO("mysql:host=localhost;dbname=petizens;charset=utf8mb4", "root", "password");
 
 date_default_timezone_set('Asia/Kuala_Lumpur');
