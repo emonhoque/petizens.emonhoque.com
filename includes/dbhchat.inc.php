3f32dc3f561c96416fc16b03a146e48c5202e5f5
emonhoque <56671915+emonhoque@users.noreply.github.com>
2023-11-18 12:38:20 -0500

    Delete includes/dbhchat.inc.php

--- includes/dbhchat.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//database_connection_chat
-
-$conn = new PDO("mysql:host=localhost;dbname=petizens;charset=utf8mb4", "root", "password");
-
-date_default_timezone_set('Asia/Kuala_Lumpur');
