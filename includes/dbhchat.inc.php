7812d1c7d07ad7062cc213652d510377bcc2de07
emonhoque <56671915+emonhoque@users.noreply.github.com>
2023-11-18 12:38:40 -0500

    Add files via upload

---
+++ includes/dbhchat.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+//database_connection_chat
+
+$conn = new PDO("mysql:host=localhost;dbname=petizens;charset=utf8mb4", "root", "password");
+
+date_default_timezone_set('Asia/Kuala_Lumpur');
